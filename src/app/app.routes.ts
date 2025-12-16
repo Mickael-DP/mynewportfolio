72cc8494a156c1c0463e9932fdea343f41086808
Mickael-DP <m.dallepasqualine@gmail.com>
2025-12-16 13:00:12 +0100

    fix: remove default router redirection to clean up URLs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,4 @@
 import { Routes } from '@angular/router';
 
 
-export const routes: Routes = [
-  { path: 'about', loadComponent: () => import('./components/about/about.component').then(m => m.AboutComponent) },
-  { path: 'experience', loadComponent: () => import('./components/experience/experience.component').then(m => m.ExperienceComponent) },
-  { path: 'projects', loadComponent: () => import('./components/projects/projects.component').then(m => m.ProjectsComponent) },
-  { path: 'skills', loadComponent: () => import('./components/skills/skills.component').then(m => m.SkillsComponent) },
-  { path: 'contact', loadComponent: () => import('./components/contact/contact.component').then(m => m.ContactComponent) },
-  { path: '', redirectTo: 'about', pathMatch: 'full' }, // Page d'accueil par défaut
-];
+export const routes: Routes = [];
